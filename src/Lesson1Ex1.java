21d18e9922542b10a0c82a5cdb20d1bebdfc5e98
30132400 <30132400@nescol.ac.uk>
2023-08-30 11:58:12 +0100

    First Commit

---
+++ src/Lesson1Ex1.java
@@ -0,0 +1,7 @@
+//Lesson 1 Exercise 1 - Display a Haiku
+public class Lesson1Ex1 {
+    public static void main(String[] args) {
+        //Write out Haiku to the console
+        System.out.println("Wildfire rages on, Catastrophe strikes the west coast, Nature's fury roars.");
+    }
+}
---
+++ src/Lesson1Ex2.java
@@ -0,0 +1,7 @@
+//Lesson 1 Exercise 2 - Display Anagrams
+ class Lesson1Ex2 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+}
