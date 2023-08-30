f60429a03996890a80af8c173e7443f7e5ee8950
olrdl <30132400@nescol.ac.uk>
2023-08-30 19:48:34 +0100

    First Commit

--- src/Lesson1Ex1.java
+++ src/Lesson1Ex1.java
@@ -2,6 +2,8 @@
 public class Lesson1Ex1 {
     public static void main(String[] args) {
         //Write out Haiku to the console
-        System.out.println("Wildfire rages on, Catastrophe strikes the west coast, Nature's fury roars.");
+        System.out.println("Wildfire rages on,");
+        System.out.println("Catastrophe strikes the west coast,");
+        System.out.println("Nature's fury shown.");
     }
 }
--- src/Lesson1Ex2.java
+++ src/Lesson1Ex2.java
@@ -1,7 +1,12 @@
 //Lesson 1 Exercise 2 - Display Anagrams
  class Lesson1Ex2 {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Here are 5 9-Letter anagrams:");
+        System.out.println("ASSERT TIN = Resistant" );
+        System.out.println("FATHER MAT = Aftermath");
+        System.out.println("IGNITED SO = Digestion");
+        System.out.println("CABLE TREE = Celebrate");
+        System.out.println("BALLET ORE = Tolerable");
     }
 
 }
