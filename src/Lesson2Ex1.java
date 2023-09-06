72e624295d016095660c63e5c7f87257ce80e2a9
30132400 <30132400@nescol.ac.uk>
2023-09-06 10:51:57 +0100

    First Commit

--- src/Lesson2Ex1.java
+++ src/Lesson2Ex1.java
@@ -4,13 +4,16 @@ public class Lesson2Ex1 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        //Scanner for Inputs
         System.out.print("What is your name? ");
         String name = input.nextLine();
+        //Input name
         System.out.print("What is your hobby? ");
         String hobby = input.nextLine();
+        //Input hobby
 
         System.out.print(name + " loves " + hobby + " in their free time. \nThey find it relaxing and satisfying to see their " + hobby + " come to life. \n" +  hobby + " allows " + name + " to express their creative side.");
-
+        //Displays Story using the user`s inputs
 
     }
 }
