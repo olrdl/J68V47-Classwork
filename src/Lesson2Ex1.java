b836d179fe5f85d58f0ded3674f10ebfc082a656
30132400 <30132400@nescol.ac.uk>
2023-09-06 10:30:31 +0100

    First Commit

--- src/Lesson2Ex1.java
+++ src/Lesson2Ex1.java
@@ -1,5 +1,7 @@
 import java.util.*;
+
 public class Lesson2Ex1 {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("What is your name? ");
