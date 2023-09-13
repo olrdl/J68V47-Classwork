aa46165730f17feea57f1461c4df125e9713ecb2
30132400 <30132400@nescol.ac.uk>
2023-09-13 12:12:10 +0100

    First Commit

--- src/Lesson3Ex3.java
+++ src/Lesson3Ex3.java
@@ -3,16 +3,17 @@ public class Lesson3Ex3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Loan Amount: ");
-        float loan = input.nextInt();
+        float loan = input.nextFloat();
 
         System.out.print("Interest rate(APR%): ");
-        float interest = input.nextInt();
+        float interest = input.nextFloat();
 
         System.out.print("Number of years: ");
         float years = input.nextInt();
-        float j = (interest/12) * 0.01;
+        float j = ((float)interest/12)/100;
         float n = years * 12;
-        float monthlyPayments = loan( j / (1- Math.pow((1+j),-n));
+        float monthlyPayments = (float)(loan*( j / (1- Math.pow((1+j),-n))));
+        //System.out.format("%f %f %f",j, n, monthlyPayments);
 
         System.out.println("Monthly Payment for this loan = " + monthlyPayments);
     }
