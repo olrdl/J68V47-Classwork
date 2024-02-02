28dfcb0545cbb3bb4b6b39bfaf99366a51b072c2
olrdl <30132400@nescol.ac.uk>
2024-02-02 19:52:46 +0000

    First Commit

--- src/Extra11Ex2.java
+++ src/Extra11Ex2.java
@@ -1,3 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Extra11Ex2 {
-    
+    public static void main(String[] args) {
+        List<String> Runners = new ArrayList<>();
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("src/runners.txt"));
+            String line = in.readLine();
+
+            while (line != null) {
+                Runners.add(line);
+                line = in.readLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error occurred reading from file: " + e.toString());
+        }
+
+        Runners.sort((s1, s2) -> {
+            String[] time1 = s1.split(", ");
+            String[] time2 = s2.split(", ");
+            return Float.compare(Float.parseFloat(time1[1]), Float.parseFloat(time2[1]));
+        });
+
+        try {
+            FileWriter out = new FileWriter("src/fastestRun.txt");
+            out.write("The fastest runner is: " + Runners.get(0));
+            out.close();
+        } catch (IOException e) {
+            System.out.println("Error occurred writing to file: " + e.toString());
+        }
+    }
 }
--- src/Extra11Ex3.java
+++ src/Extra11Ex3.java
@@ -1,2 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Extra11Ex3 {
+    public static void main(String[] args) {
+        ArrayList<String> question = new ArrayList<>();
+        ArrayList<String> answer = new ArrayList<>();
+
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("src/quiz.txt"));
+            String line;
+
+            while ((line = in.readLine()) != null) {
+                question.add(line);
+
+                if ((line = in.readLine()) != null) {
+                    answer.add(line);
+                }
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error occurred reading from file: " + e.toString());
+        }
+
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < question.size(); i++) {
+            System.out.println(question.get(i));
+            String userAnswer = input.nextLine();
+
+            if (userAnswer.equalsIgnoreCase(answer.get(i))) {
+                System.out.println("Correct!");
+            }
+            else {
+                System.out.println("Incorrect.");
+            }
+        }
+    }
 }
